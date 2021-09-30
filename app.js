590029ea179a30d6ba1d8035e488c1bc62d16729
Rina <fghjiff8@gmail.com>
2021-09-30 14:36:25 +0900

    club-upload page

--- app.js
+++ app.js
@@ -1,41 +1,45 @@
 ////모듈 추가할때마다 여기도 추가//////////////////////////////////////////////////////////
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const nunjucks = require('nunjucks');
-const { sequelize } = require('./models')
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const nunjucks = require("nunjucks");
+const { sequelize } = require("./models");
 ////라우터 추가할때마다 여기도 추가//////////////////////////////////////////////////////////
-const indexRouter = require('./routes');
-
+const indexRouter = require("./routes");
+const clubRouter = require("./routes/club");
 
 ////////////////////////////////////////////////////////////////
 const app = express();
 
 // view engine setup
-app.set('view engine', 'html');
-nunjucks.configure('views', {
+app.set("view engine", "html");
+nunjucks.configure("views", {
   express: app,
   watch: true,
-})
-sequelize.sync({ focus: false })
+});
+sequelize
+  .sync({ focus: false })
   .then(() => {
-    console.log('db 연결 성공');
+    console.log("db 연결 성공");
   })
   .catch((err) => {
     console.error(err);
   });
 ////미들웨어 추가할때마다 여기도 추가//////////////////////////////
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/img", express.static(path.join(__dirname, "uploads")));
 ////라우터 추가할때마다 여기도 추가//////////////////////////////////////////////////////////
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
+app.get("/club-upload", (req, res) => {
+  res.render("club-upload", { title: "도전클럽" });
+});
 
 ////////////////////////////////////////////////////////////////
 // catch 404 and forward to error handler
@@ -47,11 +51,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
