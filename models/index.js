3113634904906e218f596d7aa4723e063b924fc8
Rina <fghjiff8@gmail.com>
2021-09-30 10:57:06 +0900

    club db전송

--- models/index.js
+++ models/index.js
@@ -1,24 +1,27 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 ////디비형식 추가할때마다 여기도 추가//////////////////////////////////////////////////////////
 // const User = require('./user');
-
+const Club = require("./club");
 ///////////////////////////////////////////
 
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config")[env];
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 ////디비형식 추가할때마다 여기도 추가//////////////////////////////////////////////////////////
 // db.User = User;
-
+db.Club = Club;
 // User.init(sequelize);
-
+Club.init(sequelize);
 // User.associate(db);
 ///////////////////////////////////////////
 module.exports = db;
-
-
