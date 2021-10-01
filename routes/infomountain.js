0be96b4bcb5f041ddc31e4b2510ba69b7dbc8dda
Mr-butter <89626632+Mr-butter@users.noreply.github.com>
2021-10-01 21:04:55 +0900

    api 연동 데이터 추출 완료

--- routes/infomountain.js
+++ routes/infomountain.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const request = require('request');
+const APIkey = require('../public/javascripts/APIkey')
+const convert = require('xml-js');
 const router = express.Router();
+
+const servicekey = APIkey.servicekeyen
 
 /* GET page. */
 router.get('/', (req, res) => {
-  res.render('infomountain');
+
+
+  request(fullurl, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+
+      const url = 'http://apis.data.go.kr/1400000/service/cultureInfoService/mntInfoOpenAPI';
+      var queryParams = '?' + encodeURIComponent('ServiceKey') + '=' + servicekey; /* Service Key*/
+      queryParams += '&' + encodeURIComponent('searchWrd') + '='; /* */
+      queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /* */
+      queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('10'); /* */
+      const fullurl = url + queryParams;
+      console.log(fullurl);
+
+
+      data = JSON.parse(convert.xml2json(body, { compact: true, spaces: 4 }));
+      mountaindata = data.response.body.items.item
+      res.render('infomountain', { mountaindata });
+    }
+  });
+
 });
 
 module.exports = router;
