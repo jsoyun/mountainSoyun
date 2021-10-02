2d47b46be4a8b932dab40bc0000b02a714e29401
park jun hyuk <89543695+berrypjh@users.noreply.github.com>
2021-10-03 07:25:44 +0900

    메인 게시판 초안

--- routes/community.js
+++ routes/community.js
@@ -2,30 +2,30 @@ const express = require('express');
 const router = express.Router();
 const { CommunityPost, User } = require('../models');
 
-router.use((req, res, next) => {
-  // req.locals.user = req.user;
-  next();
-});
-
 /* 커뮤니티 목록 */
 router.get('/', async (req, res, next) => {
   try {
     const posts = await CommunityPost.findAll({
       include: { 
-        model: User, 
+        model: User,
         attribute: ['id', 'nick'],
       },
-      // order: ['createAt', 'DESC'],
+      order: [['createdAt', 'DESC']],
     });
     res.render('main-community', {
       title: 'mountain 커뮤니티',
       communityTwits: posts,
     });
-    console.log(posts);
   } catch (error) {
     console.error(error);
     next(error);
-  }
+  };
 });
+/* 페이지 표시 */
+// models.CommunityPost.findAll({
+//   offset: 0,
+//   limit: 100,
+//   order: "createdAt desc",
+// })
 
 module.exports = router;
---
+++ routes/view.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const { CommunityPost, User } = require('../models');
+
+/* 커뮤니티 목록 */
+router.get('/:id', async (req, res, next) => {
+  try {
+    const texts = await CommunityPost.findOne({
+      include: { 
+        model: User,
+        attribute: ['id', 'nick'],
+      },
+      where: { id: `${req.params.id}` },
+    }) 
+    console.log(texts);
+    // .then(CommunityPost => {
+        res.render('view-community', {
+            title: 'mountain 커뮤니티',
+            communityTwits: texts,
+            num: `${req.params.id}`,
+        });
+    // })
+    
+  } catch (error) {
+    console.error(error);
+    next(error);
+  };
+});
+
+module.exports = router;
