c28844ed5ee5bc603ac5dd1e30903f21eec185ea
jsoyun <syusmm233@gmail.com>
2021-10-04 12:34:01 +0900

    1004팔로잉진행

--- routes/mypage.js
+++ routes/mypage.js
@@ -1,8 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { User } = require("../models");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
+
+router.use((req, res, next) => {
+  res.locals.user = req.user;
+  res.locals.followerCount = req.user ? req.user.Followers.length : 0;
+  res.locals.followingCount = req.user ? req.user.Followings.length : 0;
+  res.locals.followerIdList = req.user
+    ? req.user.Followings.map((f) => f.id)
+    : [];
+  next();
+});
+
+router.get("/", async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ["id", "nick"],
+      },
+      order: [["createdAt", "DESC"]],
+    });
+    res.render("main", {
+      title: "NodeBird",
+      twits: posts,
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
 
 /* GET page. */
-router.get('/', (req, res) => {
-  res.render('mypage', { title: '내 정보' });
+router.get("/", (req, res) => {
+  res.render("mypage", { title: "내 정보" });
 });
 module.exports = router;
