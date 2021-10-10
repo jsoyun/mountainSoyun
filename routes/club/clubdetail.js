1463bbd7f1772aef67027be81cd2e75a31515e6b
Mr-butter <89626632+Mr-butter@users.noreply.github.com>
2021-10-10 13:14:03 +0900

    댓글 기능업데이트
    작은 수정사항

--- routes/club/clubdetail.js
+++ routes/club/clubdetail.js
@@ -17,164 +17,81 @@ router.use((req, res, next) => {
 /* 피드 상세페이지 읽기 */
 router.get("/", async (req, res, next) => {
   try {
-    const uploads = await Club.findAll({
+    res.render("club/clubdetail");
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+/* 댓글자료제공 */
+router.get("/comment", async (req, res, next) => {
+  try {
+    const reply = await Club.findAll({
       include: [{
         model: User,
         attributes: ["id", "nick"],
       },
       {
         model: ClubComment,
-        attributes: ["id", "clubId","writerId","comment","createdAt"],
-      },],
-      order: [["createdAt", "DESC"]],
-    });
-    // res.status(201).json(uploads);
-    res.render("club/clubdetail", {
-      title: "feed detail",
-      twits: uploads,
-    });
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
-
-router.get("/comment", async (req, res, next) => {
-  try {
-    const coments = await ClubComment.findAll({
-      include: [
-        {
+        attributes: ["id", "comment", "createdAt"],
+        include: {
           model: User,
           attributes: ["id", "nick"],
         },
-        {
-          model: Club,
-          attributes: ["id", "userId"],
-        },
-      ],
+      }],
+      order: [["createdAt", "DESC"]],
     });
-    res.json(coments);
+    console.log("//////////////////////////////////////////////////////////////////////////")
+    res.json(reply);
   } catch (err) {
     console.error(err);
     next(err);
   }
 });
-// router.get("/", async (req, res, next) => {
-//   try {
-//     const uploads = await Club.findAll({
-//       include: {
-//         model: User,
-//         attributes: ["id", "nick"],
-//       },
-//       order: [["createdAt", "DESC"]],
-//     });
-//     // res.status(201).json(comments);
-//     res.render("club/clubdetail", {
-//       title: "feed detail",
-//       twits: uploads,
-//     });
-//     // res.status(201).json(req.body);
-//   } catch (err) {
-//     console.error(err);
-//     next(err);
-//   }
-// });
 
-// router.get("/comment", async (req, res, next) => {
-//   try {
-//     const coments = await ClubComment.findAll({
-//       include: [
-//         {
-//           model: User,
-//           attributes: ["id", "nick"],
-//         },
-//         {
-//           model: Club,
-//           attributes: ["id", "userId"],
-//         },
-//       ],
-//     });
-//     res.json(coments);
-//   } catch (err) {
-//     console.error(err);
-//     next(err);
-//   }
-// });
+/* 댓글수정 */
+router.patch("/commentedit", isLoggedIn, async (req, res, next) => {
+  try {
+    console.log(req.body)
+    const result = await ClubComment.update({
+      comment: req.body.comment,
+    }, {
+      where: { id: req.body.id },
+    });
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+})
 
-/* 댓글 */
+/* 댓글삭제 */
+router.delete("/delete/:id", isLoggedIn, async (req, res, next) => {
+  try {
+    const result = await ClubComment.destroy({ where: { id: req.params.id } });
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+
+/* 댓글추가 */
 router.post("/", isLoggedIn, async (req, res, next) => {
   try {
-    // res.status(201).json(req.body);
     const comment = await ClubComment.create({
       clubId: req.body.clubId,
-      writerId: req.user.id,
+      writerId: req.body.writerId,
       comment: req.body.comment,
     });
-    // res.location("/");
-    res.redirect("/clubdetail");
-    // res.send("success");
+    res.send("success");
   } catch (err) {
     console.error(err);
     next(err);
   }
 });
 
-
-/* 마이페이지로에서 user가 본인 피드 삭제시 */
-router.get("/:id/delete", async (req, res, next) => {
-  console.log(req.body);
-  try {
-    await Club.destroy({ where: { id: `${req.params.id}` } });
-    res.redirect("mypage/mypage");
-  } catch (error) {
-    console.error(err);
-    next(err);
-  }
-});
-
-// router.route('/:id)
-// .get(async (req, res, next) => {
-//   try {
-//   const clubcomments = await ClubComment.findAll();
-//   res.render('clubcomments', { title: '댓글', clubcomments });
-// } catch (err) {
-//   console.error(err);
-//   next(err);
-// })
-
-
-
-// 수정시
-// router.route("/:id").patch(async (req, res, next) => {
-//   try {
-//     const result = await Comment.update(
-//       {
-//         comment: req.body.comment,
-//       },
-//       {
-//         where: { id: req.params.id },
-//       }
-//     );
-//     res.json(result);
-//   } catch (err) {
-//     console.error(err);
-//     next(err);
-//   }
-// });
-// router.get("/:id/comment", async (req, res, next) => {
-//   try {
-//     const users = await User.findAll({
-//       include: {
-//         model: User,
-//         attributes: ["nick"],
-//       },
-//       order: [["createdAt", "DESC"]],
-//     });
-//     res.render("sequelize", { users });
-//   } catch (err) {
-//     console.error(err);
-//     next(err);
-//   }
-// });
 
 module.exports = router;
