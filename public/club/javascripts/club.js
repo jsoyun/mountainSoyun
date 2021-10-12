72df062da48ca63dda1c2041ffc9c383756ef157
Rina <fghjiff8@gmail.com>
2021-10-12 17:57:45 +0900

    css수정중

--- public/club/javascripts/club.js
+++ public/club/javascripts/club.js
@@ -38,10 +38,10 @@ function addTags() {
 
 input.addEventListener("keyup", function (e) {
   if (e.key === "Enter") {
-    // const tag = createTag(input.value);
+    const tag = createTag(input.value);
     tags.push(input.value);
     addTags();
-    // tagContainer.prepend(tag);
+    tagContainer.prepend(tag);
 
     input.value = "";
   }
