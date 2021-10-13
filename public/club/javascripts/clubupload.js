f16c7ded9b534fb19b02a25c9c8217a7f8bca810
Mr-butter <89626632+Mr-butter@users.noreply.github.com>
2021-10-13 20:57:24 +0900

    이미지 여러장 업러드 오류 수정 및 업로드 태그정리

--- public/club/javascripts/clubupload.js
+++ public/club/javascripts/clubupload.js
@@ -1,7 +1,7 @@
 if (document.getElementById("img")) {
   let uploadNum = 0; //올린 사진 셀 변수 
   let index = 0; //img 에 붙일 index
-  document.getElementById("img").addEventListener("change", function (e) {
+  document.getElementById("img").addEventListener("change", async function (e) {
     const formData = new FormData();
     const length = this.files.length;
     const max = 4; //사진 최대 4장까지
@@ -38,16 +38,14 @@ if (document.getElementById("img")) {
         alert("사진은 최대 4장까지만 가능합니다.");
         return;
     }
-    console.log("업로드한 사진 : ", uploadNum);
-    console.log("현재 올린 사진 : ", this.files);
 
     for (let i = 0; i < length; i++) {
-      console.log(this.files[i]);
-      formData.append("img", this.files[i]);
+      const imgfiles = await this.files[i];
+      formData.append("img", imgfiles);
     }
     axios.post("/clubupload/img", formData)
-      .then((res) => {
-        let url = JSON.parse(res.data);
+      .then(async (res) => {
+        let url = await JSON.parse(res.data);
         console.log(url);
         let img_html = "";
         for (let i = 0; i < url.length; i++) {
@@ -56,9 +54,6 @@ if (document.getElementById("img")) {
           <img id="img-preview${index}" src="${url[i]}" width="250" alt="미리보기"> 
           <input id="img-url" type="hidden" name="url" value="${url[i]}"> 
           </div>`;
-          console.log("json 길이 : ", url.length);
-          console.log("서버통신index:", index);
-          console.log(img_html);
         }
         document.querySelector('.img-preview').innerHTML = img_html;
       })
@@ -67,42 +62,3 @@ if (document.getElementById("img")) {
       });
   });
 }
-
-// //별점 마킹 모듈 프로토타입으로 생성
-// function Rating() { };
-// Rating.prototype.rate = 0;
-// Rating.prototype.setRate = function (newrate) {
-//   //별점 마킹 - 클릭한 별 이하 모든 별 체크 처리
-//   this.rate = newrate;
-//   let items = document.querySelectorAll('.rate_radio');
-//   items.forEach(function (item, idx) {
-//     if (idx < newrate) {
-//       item.checked = true;
-//     } else {
-//       item.checked = false;
-//     }
-//   });
-// }
-// let rating = new Rating();//별점 인스턴스 생성
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   //별점선택 이벤트 리스너
-//   document.querySelector('.rating').addEventListener('click', function (e) {
-//     let elem = e.target;
-//     if (elem.classList.contains('rate_radio')) {
-//       rating.setRate(parseInt(elem.value));
-//     }
-//   })
-// });
-
-// /* 별점 */
-// const container = document.querySelector(".rating");
-// const items = container.querySelectorAll(".rating-item");
-// container.onclick = (e) => {
-//   const elClass = e.target.classList;
-//   if (!elClass.contains("active")) {
-//     items.forEach((item) => item.classList.remove("active"));
-//     console.log(e.target.getAttribute("data-rate"));
-//     elClass.add("active");
-//   }
-// };
--- routes/club/clubupload.js
+++ routes/club/clubupload.js
@@ -39,7 +39,8 @@ const upload = multer({
 });
 
 router.post("/img", isLoggedIn, upload.array("img", 4), (req, res) => {
-  console.log("파일" + req.file);
+  // console.log("//////////////////////////////////////////////////////////");
+  // console.log(req.files);
   let urlArr = new Array();
   for (let i = 0; i < req.files.length; i++) {
     urlArr.push(`/img/${req.files[i].filename}`);
@@ -51,40 +52,74 @@ router.post("/img", isLoggedIn, upload.array("img", 4), (req, res) => {
 
 router.post("/", isLoggedIn, upload.array("img", 4), async (req, res, next) => {
   try {
-    if (req.body.url == false) {
+    const URL = await req.body.url;
+    console.log("////////////////////////////////////////////")
+    console.log(typeof URL)
+    console.log(URL.length)
+    console.log("////////////////////////////////////////////")
+    if (URL == false) {
       return res.send("<script>alert('이미지를 업로드해주세요.'); location.href='/clubupload';</script>");
     }
+    if (typeof URL == "object") {
+      console.log("오브젝트코드실행")
+      const club = await Club.create({
+        content: req.body.content,
+        hash: req.body.hashtag,
+        img: URL[0],
+        star: req.body.star,
+        userId: req.user.id,
+      });
 
-    let URL = req.body.url;
-    const club = await Club.create({
-      content: req.body.content,
-      hash: req.body.hashtag,
-      img: URL[0],
-      star: req.body.star,
-      userId: req.user.id,
-    });
+      for (let i = 0; i < (URL.length - 1); i++) {
+        await Img.create({
+          img: URL[i],
+          clubImgId: club.id,
+        })
+      };
 
-    for (let i = 0; i < (URL.length-1); i++) {
+      const hashtags = req.body.hashtag.match(/#[^\s#]*/g);
+      console.log(hashtags);
+      if (hashtags) {
+        const result = await Promise.all(
+          hashtags.map((tag) => {
+            return Hashtag.findOrCreate({
+              where: { title: tag.slice(1).toLowerCase() },
+            });
+          })
+        );
+        console.log(result);
+        await club.addHashtag(result.map((r) => r[0]));
+      }
+      res.redirect("/club");
+      return
+    } else {
+      console.log("스트링코드실행")
+      const club = await Club.create({
+        content: req.body.content,
+        hash: req.body.hashtag,
+        img: URL,
+        star: req.body.star,
+        userId: req.user.id,
+      });
       await Img.create({
-        img: URL[i],
+        img: URL,
         clubImgId: club.id,
       })
-    };
-
-    const hashtags = req.body.hashtag.match(/#[^\s#]*/g);
-    console.log(hashtags);
-    if (hashtags) {
-      const result = await Promise.all(
-        hashtags.map((tag) => {
-          return Hashtag.findOrCreate({
-            where: { title: tag.slice(1).toLowerCase() },
-          });
-        })
-      );
-      console.log(result);
-      await club.addHashtag(result.map((r) => r[0]));
+      const hashtags = req.body.hashtag.match(/#[^\s#]*/g);
+      console.log(hashtags);
+      if (hashtags) {
+        const result = await Promise.all(
+          hashtags.map((tag) => {
+            return Hashtag.findOrCreate({
+              where: { title: tag.slice(1).toLowerCase() },
+            });
+          })
+        );
+        console.log(result);
+        await club.addHashtag(result.map((r) => r[0]));
+      }
+      res.redirect("/club");
     }
-    res.redirect("/club");
   } catch (error) {
     console.error(error);
     next(error);
