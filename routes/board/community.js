8e35dce5f878deddff9b6b9383192aa58d33efbc
Mr-butter <89626632+Mr-butter@users.noreply.github.com>
2021-10-13 16:39:44 +0900

    게시판 댓글수 표시

--- routes/board/community.js
+++ routes/board/community.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const url = require('url');
-const { CommunityPost, User } = require('../../models');
+const { CommunityPost, User, Communitycomment } = require('../../models');
 
 router.use((req, res, next) => {
   res.locals.user = req.user;
@@ -21,16 +21,19 @@ router.get('/page', async (req, res, next) => {
     let limit = Math.max(1, parseInt(queryData.limit));   // 표시될 게시물 수
     let count = await CommunityPost.findAll();
     let allBoard = count.length;                    // 전체 게시글 수
-    let maxPage = Math.ceil(allBoard/limit);           // 전체 / 보여줄 페이지 = 마지막 페이지
-    let nowPage = Math.ceil(offset/limit);          // 현재 페이지
-    offset = !isNaN(offset)?offset:0;
-    limit = !isNaN(limit)?limit:10;
-    
+    let maxPage = Math.ceil(allBoard / limit);           // 전체 / 보여줄 페이지 = 마지막 페이지
+    let nowPage = Math.ceil(offset / limit);          // 현재 페이지
+    offset = !isNaN(offset) ? offset : 0;
+    limit = !isNaN(limit) ? limit : 10;
+
     const posts = await CommunityPost.findAll({
-      include: { 
+      include: [{
         model: User,
         attribute: ['id', 'nick'],
-      },
+      }, {
+        model: Communitycomment,
+        attribute: ['commenterId', 'comment'],
+      },],
       order: [['id', 'DESC']],
       limit: limit,    // 페이지에 표시될 게시물 수
       offset: offset,    // 시작 지점
