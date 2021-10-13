0742c8d857b0bfb88692e844c69334fb08b456dc
Mr-butter <89626632+Mr-butter@users.noreply.github.com>
2021-10-13 23:16:23 +0900

    명산정보 처름부터 표기

--- public/mountainInfo/javascripts/APIdata.js
+++ public/mountainInfo/javascripts/APIdata.js
@@ -1,3 +1,60 @@
+firstload()
+async function firstload() {
+    const mountainList = document.querySelector('#mountain-list');
+    let location = "경기";
+    const apidata1 = await axios.post("/infomountain/location", { location });
+    console.log(apidata1);
+    location = "인천";
+    const apidata2 = await axios.post("/infomountain/location", { location });
+    arrydata1 = JSON.stringify(apidata1.data);
+    arrydata2 = JSON.stringify(apidata2.data);
+    sumarrydata = arrydata1.slice(0, -1) + "," + arrydata2 + "]";
+    const apidata = JSON.parse(sumarrydata);
+    console.log(apidata);
+    mountainList.innerHTML = "";
+    for (let i = 0; i < apidata.length; i++) {
+        let listContainer = document.createElement("div")
+        listContainer.setAttribute('class', "mountain-list-container");
+        mountainList.appendChild(listContainer);
+
+        let listItem = document.createElement("div");
+        listItem.setAttribute('class', "mountain-name")
+        listItem.textContent = apidata[i].mntnm
+        listContainer.appendChild(listItem);
+
+        listItem = document.createElement("div");
+        listItem.setAttribute('class', "mountain-height")
+        listItem.textContent = apidata[i].mntheight
+        listContainer.appendChild(listItem);
+
+        listItem = document.createElement("div");
+        listItem.setAttribute('class', "mountain-address")
+        listItem.innerHTML = apidata[i].areanm
+        listContainer.appendChild(listItem);
+
+        listItem = document.createElement("div");
+        listItem.setAttribute('class', "mountain-overview")
+        listItem.innerHTML = apidata[i].overview
+        listContainer.appendChild(listItem);
+
+        listItem = document.createElement("div");
+        listItem.setAttribute('class', "mountain-transport")
+        listItem.innerHTML = apidata[i].transport
+        listContainer.appendChild(listItem);
+
+        listItem = document.createElement("div");
+        listItem.setAttribute('class', "mountain-tour")
+        listItem.innerHTML = apidata[i].tourisminf
+        listContainer.appendChild(listItem);
+
+        listItem = document.createElement("div");
+        listItem.setAttribute('class', "mountain-etctour")
+        listItem.innerHTML = apidata[i].etccourse
+        listContainer.appendChild(listItem);
+    }
+    return
+};
+
 document.querySelectorAll(".kor-location").forEach(element => {
     element.addEventListener('click', async function () {
         let location = await this.textContent;
